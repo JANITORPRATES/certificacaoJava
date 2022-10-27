3736986508d999c529cad558bd811b21bc4a6700
Janitor Prates <janitorprates@gmail.com>
2022-10-26 21:25:49 -0300

    classes de teste

---
+++ scr/Electronic.java
@@ -0,0 +1,22 @@
+package scr;
+
+public class Electronic implements Device
+{ 
+	public void doIt() { }
+}
+
+abstract class Phone1 extends Electronic { }
+
+abstract class Phone2 extends Electronic
+{
+	public abstract void doIt(int x);
+}
+
+class Phone3 extends Electronic implements Device
+{ 
+	public void doStuff() { }
+	public void doIt(){}
+}
+interface Device { 
+	abstract void doIt();
+ }
---
+++ scr/Teste.java
@@ -0,0 +1,50 @@
+package scr;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+//import java.text.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+//import java.util.*;
+import java.util.logging.FileHandler;
+import java.util.logging.SimpleFormatter;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+//import java.util.logging.*;
+
+public class Teste {
+	
+	public static void main(String[] args) throws IOException { 
+	
+		/* Assegura que o diretório seja criado */ 
+		Files.createDirectories(Paths.get("E:/CODIGOS/certificacaoJava/logs"));
+		//Files.createDirectories(Paths.get());
+
+		 /* Obtém a data a ser usada no nome do arquivo */
+		 DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyyMMdd_hhmm");
+		 LocalDateTime now = LocalDateTime.now();
+		 String date = now.format(df);
+
+		/* Define o nome do arquivo no diretório logs */ 
+		String logFileName = "logs\\testlog-" + date + ".txt"; 
+
+		/* Define Logger */
+		FileHandler myFileHandler = new FileHandler(logFileName);
+		myFileHandler.setFormatter(new SimpleFormatter());
+
+		Logger ocajLogger = Logger.getLogger("OCAJ Logger");
+		ocajLogger.setLevel(Level.ALL);
+		ocajLogger.addHandler(myFileHandler); 
+		
+		/* Mensagem de log */
+		ocajLogger.info("\nThis is a logged information message. ");
+
+		/* Fecha o arquivo */ 
+		myFileHandler.close();
+	} 
+}
+
+		
