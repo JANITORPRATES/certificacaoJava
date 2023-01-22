4f0f8ade802907816eeb6544f9b36c8d29494132
Janitor Prates <janitorprates@gmail.com>
2023-01-22 15:22:14 -0300

    IllegalArgumentException na classe Conta3

--- scr/ALURA/Excecoes/Conta3.java
+++ scr/ALURA/Excecoes/Conta3.java
@@ -4,39 +4,43 @@ public abstract class Conta3 {
 	private int agencia;
 	private int numero;
 	private double saldo;
-	
+
 	public Conta3(int agencia, int numero) {
-		
+
+		if (agencia < 1) {
+			throw new IllegalArgumentException("Agencia inválida");
+		}
+
+		if (numero < 1) {
+			throw new IllegalArgumentException("Numero da conta inválido");
+		}
 		this.agencia = agencia;
 		this.numero = numero;
-		
+
 	}
-	
-	
+
 	public double getSaldo() {
 		return saldo;
 	}
-
 
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	public void saca(double valor) throws SaldoInsuficienteException {
 
-	public void saca(double valor) throws SaldoInsuficienteException{
+		if (this.saldo < valor) {
+			throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
+		}
 
-        if(this.saldo < valor) {
-            throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);
-        } 
-
-        this.saldo -= valor;       
-}
-	
-	public void transfere(double valor, Conta3 destino) throws SaldoInsuficienteException{
-	    this.saca(valor);
-	    destino.deposita(valor);
+		this.saldo -= valor;
 	}
-	
+
+	public void transfere(double valor, Conta3 destino) throws SaldoInsuficienteException {
+		this.saca(valor);
+		destino.deposita(valor);
+	}
+
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
