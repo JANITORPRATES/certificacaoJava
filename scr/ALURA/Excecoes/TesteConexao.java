6b6c696822fc431cea100d827397965ac345c390
Janitor Prates <janitorprates@gmail.com>
2023-01-22 15:17:13 -0300

    implementando a interface AutoCloseable e o método close() = try with resources

--- scr/ALURA/Excecoes/TesteConexao.java
+++ scr/ALURA/Excecoes/TesteConexao.java
@@ -4,15 +4,22 @@ public class TesteConexao {
 
     public static void main(String[] args)  {
 
-    	Conexao con = null;
-    	try{
-    	    con = new Conexao();
-    	    con.leDados();
-    	    con.fecha();
+//    	Conexao con = null;
+//    	try{
+//    	    con = new Conexao();
+//    	    con.leDados();
+//    	    con.fecha();
+//    	} catch(IllegalStateException ex){
+//    	    System.out.println("Deu erro na conexão");
+//    	} finally {
+//    	    con.fecha();
+//    	}
+    	
+    	
+    	try(Conexao conexao = new Conexao() ){
+    	    conexao.leDados();
     	} catch(IllegalStateException ex){
     	    System.out.println("Deu erro na conexão");
-    	} finally {
-    	    con.fecha();
     	}
 
     }
