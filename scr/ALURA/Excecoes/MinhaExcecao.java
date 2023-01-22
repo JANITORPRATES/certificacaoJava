a386424d58649e6b9cdfd7ead89992f56049f021
Janitor Prates <janitorprates@gmail.com>
2023-01-22 13:26:31 -0300

    diferença entre exceção verificada e não verificada

--- scr/ALURA/Excecoes/MinhaExcecao.java
+++ scr/ALURA/Excecoes/MinhaExcecao.java
@@ -1,7 +1,17 @@
 package scr.ALURA.Excecoes;
 
+//uncheck - não verificada pelo compilador
 public class MinhaExcecao extends RuntimeException {
     public MinhaExcecao(String msg) {
         super(msg);
     }
 }
+
+
+
+//check - verificada pelo compilador
+//public class MinhaExcecao extends Exception {
+//  public MinhaExcecao(String msg) {
+//      super(msg);
+//  }
+//}
