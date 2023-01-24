0711f11f7c7a6a568a5392d218f3b2d9fc0c7d5c
Janitor Prates <janitorprates@gmail.com>
2023-01-23 22:49:23 -0300

    Recriando o ArrayList

---
+++ scr/ALURA/JavaUtil/TesteGuardadorDeContas.java
@@ -0,0 +1,25 @@
+package scr.ALURA.JavaUtil;
+
+import scr.ALURA.BancoOrientadoObjeto.Contas.Conta;
+import scr.ALURA.BancoOrientadoObjeto.Contas.ContaCorrente;
+
+public class TesteGuardadorDeContas {
+
+      public static void main(String[] args) {
+
+              GuardadorDeContas guardador = new GuardadorDeContas();
+
+              Conta cc = new ContaCorrente(22, 11);
+              guardador.adiciona(cc);
+
+              Conta cc2 = new ContaCorrente(22, 22);
+              guardador.adiciona(cc2);
+
+              int tamanho = guardador.getQuantidadeDeElementos();
+              System.out.println(tamanho);
+              
+              Conta ref = guardador.getReferencia(0);
+              System.out.println(ref.getNumero());
+
+      }
+}
