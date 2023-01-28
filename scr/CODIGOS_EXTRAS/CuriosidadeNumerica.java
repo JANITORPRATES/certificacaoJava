79e223b17e640a8fbb9d40f7128a13ed4f3451bf
Janitor Prates <janitorprates@gmail.com>
2023-01-28 14:49:09 -0300

    uso do underscore nos números - resumo só pode ser usado se dos dois lados do underscore tem um literal númerico

--- scr/CODIGOS_EXTRAS/CuriosidadeNumerica.java
+++ scr/CODIGOS_EXTRAS/CuriosidadeNumerica.java
@@ -33,6 +33,49 @@ public class CuriosidadeNumerica {
 		int valorHexadecimal = 0x1a;//começa com zero xis
 		int valorOctal = 032; //começa com zero
 		int valorBinario = 0b11010;//começa com zero bê
+		
+		
+		long numeroCartaoCredito = 1234_5678_9012_3456L;
+		long cpf = 101_134_156_68l;
+		float pi = 3.14_15F;
+		long hexBytes = 0xFF_EC_DE_5E;
+		long hexWords = 0xCAFE_BABE;
+		long maxLong = 0x7fff_ffff_ffff_ffffl;
+		byte nybbles = 0b0010_0101;
+		long bytes = 0b11010010_01101001_10010100_101010;
+		
+		/*
+		//Não pode colocar underscore perto de ponto
+		float pi1 = 3_.1415f;
+		float pi2 = 3._1415f;
+		
+		//Não pode colocar underscore perto de sufixos (L)
+		long cpf = 101_134_156_68_L;
+		
+		//isso é um identificador e não um literal
+		int x1 = _52;
+		
+		//OK (decimal literal)
+		int x2 = 5_2;
+		int x = 5____________2;
+		
+		//Não pode colocar underscore no final de um literal
+		int x3 = 52_
+		
+		
+		//Não pode colocar underscore perto de prefixos (0x)
+		int x5 = 0_x52;
+		
+		
+		//Não pode colocar underscore no começo e nem no final de números
+		int x6 = 0x_52;
+		int x8 = 0x52_;
+		
+		
+		//OK (hexadecimal literal)
+		int x7 = 0x5_2;
+		
+		*/
 	}
 
 }
