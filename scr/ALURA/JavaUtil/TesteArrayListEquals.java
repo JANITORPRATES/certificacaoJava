abe1be135f1841c424622e9937c4a8be8225f1b7
Janitor Prates <janitorprates@gmail.com>
2023-01-28 10:47:38 -0300

    Contas agora são consideradas iguais quando agência e número da conta são os mesmos

--- scr/ALURA/JavaUtil/TesteArrayListEquals.java
+++ scr/ALURA/JavaUtil/TesteArrayListEquals.java
@@ -8,25 +8,31 @@ import scr.ALURA.BancoOrientadoObjeto.Contas.ContaCorrente;
 public class TesteArrayListEquals {
 
         public static void main(String[] args) {
+        	
+        	Conta cc1 = new ContaCorrente(22, 22);
+            Conta cc2 = new ContaCorrente(22, 22);
 
-        //Generics
-        ArrayList<Conta> lista = new ArrayList<Conta>();
+            boolean igual = cc1.ehIgual(cc2);
+            System.out.println(igual);
 
-        Conta cc = new ContaCorrente(22, 11);
-        lista.add(cc);
-
-        Conta cc2 = new ContaCorrente(22, 22);
-        lista.add(cc2);
-
-        Conta cc3 = new ContaCorrente(22, 22);        
-        boolean existe = lista.contains(cc3);
-
-        System.out.println("Já existe? " + existe);
-
-        for(Conta conta : lista) {
-            if(conta.ehIgual(cc3)) {
-                    System.out.println("Já tenho essa conta!");
-            }
-    }
+//        //Generics
+//        ArrayList<Conta> lista = new ArrayList<Conta>();
+//
+//        Conta cc = new ContaCorrente(22, 11);
+//        lista.add(cc);
+//
+//        Conta cc2 = new ContaCorrente(22, 22);
+//        lista.add(cc2);
+//
+//        Conta cc3 = new ContaCorrente(22, 22);        
+//        boolean existe = lista.contains(cc3);
+//
+//        System.out.println("Já existe? " + existe);
+//
+//        for(Conta conta : lista) {
+//            if(conta.ehIgual(cc3)) {
+//                    System.out.println("Já tenho essa conta!");
+//            }
+//    }
     }
 }
