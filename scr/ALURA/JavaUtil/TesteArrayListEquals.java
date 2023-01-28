0b7acc9a3fc3c2b3592b3a2e46d95e21d0b95569
Janitor Prates <janitorprates@gmail.com>
2023-01-28 10:26:18 -0300

    Testando o método contains

---
+++ scr/ALURA/JavaUtil/TesteArrayListEquals.java
@@ -0,0 +1,29 @@
+package scr.ALURA.JavaUtil;
+
+import java.util.ArrayList;
+
+import scr.ALURA.BancoOrientadoObjeto.Contas.Conta;
+import scr.ALURA.BancoOrientadoObjeto.Contas.ContaCorrente;
+
+public class TesteArrayListEquals {
+
+        public static void main(String[] args) {
+
+        //Generics
+        ArrayList<Conta> lista = new ArrayList<Conta>();
+
+        Conta cc = new ContaCorrente(22, 11);
+        lista.add(cc);
+
+        Conta cc2 = new ContaCorrente(22, 22);
+        lista.add(cc2);
+
+        boolean existe = lista.contains(cc2);
+
+        System.out.println("Já existe? " + existe);
+
+        for(Conta conta : lista) {
+                System.out.println(conta);
+        }
+    }
+}
