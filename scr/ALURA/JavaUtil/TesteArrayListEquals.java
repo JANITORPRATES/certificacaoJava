b4d990157ae6fe4a09f3682c458bf2fc9d90c0cb
Janitor Prates <janitorprates@gmail.com>
2023-01-28 10:29:54 -0300

    O operador == (igual igual) compara o valor de referência

--- scr/ALURA/JavaUtil/TesteArrayListEquals.java
+++ scr/ALURA/JavaUtil/TesteArrayListEquals.java
@@ -18,12 +18,15 @@ public class TesteArrayListEquals {
         Conta cc2 = new ContaCorrente(22, 22);
         lista.add(cc2);
 
-        boolean existe = lista.contains(cc2);
+        Conta cc3 = new ContaCorrente(22, 22);        
+        boolean existe = lista.contains(cc3);
 
         System.out.println("Já existe? " + existe);
 
         for(Conta conta : lista) {
-                System.out.println(conta);
-        }
+            if(conta == cc3) {
+                System.out.println("Já tenho essa conta!");
+            }
+    }
     }
 }
